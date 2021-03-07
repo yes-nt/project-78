1e0dc56fe9715233f3d524489ca66e3054406167
yes-nt <70011254+yes-nt@users.noreply.github.com>
2021-03-07 12:32:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import SignUpLoginScreen from './screens/signuploginscreen';
+
+export default function App() {
+  return (
+    <View>
+      <SignUpLoginScreen />
+    </View>
+  );
+}
